8e7ae83bb99e730bf67a5956204996693d00de5c
jposes <32463635+jposes@users.noreply.github.com>
2023-03-27 15:31:06 -0600

    Update main.js

--- PreEntrega1_OsesNunez/js/main.js
+++ PreEntrega1_OsesNunez/js/main.js
@@ -25,108 +25,74 @@ Guardar la seleccion del usuario y continuar
 
 */
 
-
-//1
 let nombre = prompt("Bienvenido a nuestro servicio de Rent a Car! Ingresa tu nombre por favor:");
 alert("¡Hola " + nombre + "! Bienvenido/a a nuestro Rent a Car.");
 
-//2
 let modelo = "";
 let costoPorDia;
-while (!modelo) {
-  let seleccionUsuario = prompt(
-    "Seleccione el modelo de vehículo que necesita:\n" +
-      "a- Sedans\n" +
-      "b- SUV\n" +
-      "c- 4x4\n" +
-      "d- Special 4x4"
-  );
-  switch (seleccionUsuario) {
-    case "a":
-      modelo = "Sedans";
-      costoPorDia = 100;
-      break;
-    case "b":
-      modelo = "SUV";
-      costoPorDia = 200;
-      break;
-    case "c":
-      modelo = "4x4";
-      costoPorDia = 150;
-      break;
-    case "d":
-      modelo = "Special 4x4";
-      costoPorDia = 300;
-      break;
-  }
-}
-
-//3
 let dias = "";
+let costoTotal;
 
-while (!dias) {
-  let seleccionUsuario = prompt("¿Cuántos días necesita alquilar el vehículo?");
-
-  if (!seleccionUsuario || isNaN(seleccionUsuario)) {
-    alert("Lo siento, '" + seleccionUsuario + "' no es un número válido.");
-  } else {
-    dias = seleccionUsuario;
-  }
+function calcularCostoTotal(dias, costoPorDia) {
+  return dias * costoPorDia;
 }
 
-//4
-let costoTotal = dias * costoPorDia;
+function alquilarVehiculo() {
+  while (!modelo) {
+    let seleccionUsuario = prompt(
+      "Seleccione el modelo de vehículo que necesita:\n" +
+        "a- Sedans - costo por dia $100\n" +
+        "b- SUV - costo por dia $200\n" +
+        "c- 4x4 - costo por dia $350\n" +
+        "d- Special 4x4 - costo por dia $400"
+    );
+    switch (seleccionUsuario) {
+      case "a":
+        modelo = "Sedans";
+        costoPorDia = 100;
+        break;
+      case "b":
+        modelo = "SUV";
+        costoPorDia = 200;
+        break;
+      case "c":
+        modelo = "4x4";
+        costoPorDia = 350;
+        break;
+      case "d":
+        modelo = "Special 4x4";
+        costoPorDia = 400;
+        break;
+    }
+  }
 
-//5
+  while (!dias) {
+    let seleccionUsuario = prompt("¿Cuántos días necesita alquilar el vehículo?");
+
+    if (!seleccionUsuario || isNaN(seleccionUsuario)) {
+      alert("Lo siento, '" + seleccionUsuario + "' no es un número válido.");
+    } else {
+      dias = seleccionUsuario;
+    }
+  }
+
+  costoTotal = calcularCostoTotal(dias, costoPorDia);
+}
+
+alquilarVehiculo();
+
 let confirmacion = false;
+
 while (!confirmacion) {
-  let seleccionUsuario = prompt(
-    nombre + " " + "has seleccionado un " +  modelo + " para alquilar durante " + dias + " días. El costo total será $" +  costoTotal +  ". ¿Está seguro de realizar la compra? Responde 'si' o 'no'"
-  );
+  let seleccionUsuario = prompt( nombre + " " + "has seleccionado un " + modelo + " para alquilar durante " + dias + " días. El costo total será $" +  costoTotal +  ". ¿Está seguro de realizar la compra? Responde 'si' o 'no'" );
   if (seleccionUsuario.toLowerCase() === "si") {
     confirmacion = true;
     alert(nombre + " " + "¡Gracias por elegir nuestro servicio! ¡Disfruta de tu viaje!");
   } else if (seleccionUsuario.toLowerCase() === "no") {
     modelo = "";
-    while (!modelo) {
-      let newmodelo = prompt(
-        "Seleccione el modelo de vehículo que necesita:\n" +
-          "a- Sedans\n" +
-          "b- SUV\n" +
-          "c- 4x4\n" +
-          "d- Special 4x4"
-      );
-      switch (newmodelo) {
-        case "a":
-          modelo = "Sedans";
-          costoPorDia = 100;
-          break;
-        case "b":
-          modelo = "SUV";
-          costoPorDia = 200;
-          break;
-        case "c":
-          modelo = "4x4";
-          costoPorDia = 150;
-          break;
-        case "d":
-          modelo = "Special 4x4";
-          costoPorDia = 300;
-          break;
-      }
-    }
-
     dias = "";
-    while (!dias) {
-      let seleccionUsuario = prompt("¿Cuántos días necesita alquilar el vehículo?");
-    
-      if (!seleccionUsuario || isNaN(seleccionUsuario)) {
-        alert("Lo siento, '" + seleccionUsuario + "' no es un número válido.");
-      } else {
-        dias = seleccionUsuario;
-      }
-    }
-    costoTotal = dias * costoPorDia;
+    costoPorDia = "";
+    alquilarVehiculo();
   }
 }
 
